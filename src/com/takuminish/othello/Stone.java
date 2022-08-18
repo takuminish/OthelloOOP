9cc80e467a0bc8151bddc9204f8a28d11f4f54fe
takuminish <takuminv1997@gmail.com>
2022-08-18 22:42:54 +0900

    ファクトリメソッドの作成

--- src/com/takuminish/othello/Stone.java
+++ src/com/takuminish/othello/Stone.java
@@ -15,9 +15,27 @@ public class Stone {
      * @param position 石の位置
      * @param type 石の種類
      */
-    public Stone(final CellPosition position, final  StoneType type) {
+    private Stone(final CellPosition position, final  StoneType type) {
         this.position = position;
         this.type = type;
+    }
+
+    /**
+     * 白石のインスタンスを生成
+     * @param position 石の位置
+     * @return 白石のStoneインスタンス
+     */
+    static public Stone createWhiteStone(final CellPosition position) {
+        return new Stone(position, StoneType.White);
+    }
+
+    /**
+     * 黒石のインスタンスを生成
+     * @param position 石の位置
+     * @return 黒石のStoneインスタンス
+     */
+    static public Stone createBlackStone(final CellPosition position) {
+        return new Stone(position, StoneType.Black);
     }
 
 
